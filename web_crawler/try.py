350af066896d94b948ff65abdcc1b671fca1988e
lenmorld <lenmorld@gmail.com>
2016-12-01 17:16:19 -0500

    working afinn

--- web_crawler/try.py
+++ web_crawler/try.py
@@ -7,20 +7,29 @@ import jsonlines
 from afinn import Afinn
 afinn = Afinn()
 
+# afinn.setup_from_file(os.path.join(afinn.data_dir(), 'AFINN-165.txt'),
+#                       word_boundary=False)
+
+afinn.setup_from_file(os.path.join(afinn.data_dir(), 'AFINN-111.txt'),
+                      word_boundary=False)
+
 """
 get_score
 
 for now, just return length
 we're supposed to do sentiment analysis here and return the text's score
 """
-def get_score(title, text):
+def get_score(field, title, text):
 
     # title_score = afinn.score(title)
     text_score = afinn.score(text)
     # sum_score = title_score + text_score
-    print(title,text)
-    print(":", text_score)
-    return text_score
+    # print(title)
+
+    # if "mystery" in field:
+    #     print(field,title,text)
+    #     print(":", text_score)
+    return text_score, len(text)
 
 
 PYTHONIOENCODING="UTF-8"
@@ -41,6 +50,8 @@ os.chdir("../sample_output")
 # each department will have its own accumulated score
 # e.g. artsci_biology
 department_scores = {}
+department_lengths = {}
+department_page_count = {}
 
 # loop through docs and accumulate scores
 for f in glob.glob("*.jsonl"):
@@ -52,11 +63,27 @@ for f in glob.glob("*.jsonl"):
             text = format(obj['text'])
 
             try:
-                department_scores[field] += get_score(title, text)         # call method to get scores for text
-            except KeyError:  # department score not initialized yet
+                department_scores[field]
+                department_lengths[field]
+                department_page_count[field]
+            except KeyError:
                 department_scores[field] = 0         # init
+                department_lengths[field] = 0
+                department_page_count[field] = 0
+
+
+            score, length = get_score(field, title, text)         # call method to get scores for text
+            department_scores[field] += score         # call method to get scores for text
+            department_lengths[field] += length
+            department_page_count[field] += 1
+
 
 for d_s in department_scores:
-    print(d_s, ':',  department_scores[d_s])
+
+    denominator = (department_lengths[d_s] / department_page_count[d_s] ) / 100
+
+    normalized_score = department_scores[d_s] / denominator
+
+    print(d_s, '_Count:', department_page_count[d_s], ':',  department_scores[d_s], ':', normalized_score)
 
 
