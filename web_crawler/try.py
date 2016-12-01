c5a161341232a6a912919c78063c87cb69cfb506
lenmorld <lenmorld@gmail.com>
2016-12-01 14:11:53 -0500

    first commit

---
+++ web_crawler/try.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+
+import glob, os
+
+import jsonlines
+
+os.chdir("../sample_output")
+for f in glob.glob("*.jsonl"):
+    with jsonlines.open(f) as reader:
+        for obj in reader:
+            # WARNING: The string in obj are encoded with unicode.
+            print(u"Field: {}".format(obj['field']))
+            print(u"url: {}".format(obj['url']))
+            print(u"title: {}".format(obj['title']))
+            print(u"text: {}".format(obj['text']))
