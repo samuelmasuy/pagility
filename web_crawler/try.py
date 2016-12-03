23ac074d058c24a659a7c96ab0c912b716ca45a8
Daniil Karpov <daniil.karpov@hotmail.com>
2016-12-03 14:13:57 -0500

    Added afinn sentiment analysis and three-way classification for each department

--- web_crawler/try.py
+++ web_crawler/try.py
@@ -22,14 +22,15 @@ we're supposed to do sentiment analysis here and return the text's score
 def get_score(field, title, text):
 
     # title_score = afinn.score(title)
-    text_score = afinn.score(text)
+    token_score = afinn.score(text)
     # sum_score = title_score + text_score
     # print(title)
 
     # if "mystery" in field:
     #     print(field,title,text)
     #     print(":", text_score)
-    return text_score, len(text)
+    #return text_score, len(text)
+    return token_score
 
 
 PYTHONIOENCODING="UTF-8"
@@ -52,6 +53,7 @@ os.chdir("../sample_output")
 department_scores = {}
 department_lengths = {}
 department_page_count = {}
+department_token_scores = {}  # department is the key, values are dictionaries for each text in that department
 
 # loop through docs and accumulate scores
 for f in glob.glob("*.jsonl"):
@@ -62,28 +64,65 @@ for f in glob.glob("*.jsonl"):
             title = format(obj['title'])
             text = format(obj['text'])
 
-            try:
-                department_scores[field]
-                department_lengths[field]
-                department_page_count[field]
-            except KeyError:
-                department_scores[field] = 0         # init
-                department_lengths[field] = 0
-                department_page_count[field] = 0
+            data = title + text
+            #data = data.encode("ascii", "ignore")
+            data_score = get_score(field, title, data)  # call method to get a score for all data
 
+            if field not in department_scores:
+                department_scores[field] = data_score
+            else:
+                department_scores[field] += data_score
 
-            score, length = get_score(field, title, text)         # call method to get scores for text
-            department_scores[field] += score         # call method to get scores for text
-            department_lengths[field] += length
-            department_page_count[field] += 1
+            if field not in department_token_scores:
+                URL_token_scores = {}
+                department_token_scores[field] = {}
 
+            token_scores = []
+            tokens = data.split()
+            length = len(data)
+            #length = 0
+            for token in tokens:
+                score = get_score(field, title, token)  # call method to get a score for the token
+                #length += 1
+                if score != 0:
+                    token_score_tuple = (token, score)
+                    token_scores.append(token_score_tuple)
+
+            URL_token_scores[url] = token_scores
+            department_token_scores[field] = URL_token_scores
+
+            """if field in department_scores:
+                department_scores[field] += score
+            else:
+                department_scores[field] = score"""
+
+            if field in department_lengths:
+                department_lengths[field] += length
+            else:
+                department_lengths[field] = length
+
+            if field in department_page_count:
+                department_page_count[field] += 1
+            else:
+                department_page_count[field] = 1
+
+for dep in department_token_scores:
+    print()
+    print(dep, ":")
+    score = 0
+    for doc in department_token_scores[dep]:
+        print(doc, ":")
+        print(department_token_scores[dep][doc])
+        for token_tuple in department_token_scores[dep][doc]:
+            score += token_tuple[1]
+    print("Total score:", score)
 
 for d_s in department_scores:
 
-    denominator = (department_lengths[d_s] / department_page_count[d_s] ) / 100
+    denominator = (department_lengths[d_s] / department_page_count[d_s]) / 100
 
     normalized_score = department_scores[d_s] / denominator
 
-    print(d_s, '_Count:', department_page_count[d_s], ':',  department_scores[d_s], ':', normalized_score)
+    print(d_s, '_Count:', department_page_count[d_s], ':',  department_scores[d_s], ':', normalized_score, ':', department_lengths[d_s])
 
 
